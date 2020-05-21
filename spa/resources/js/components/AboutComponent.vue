2434172848530ebac96907edbbbe15bf463f1ab6
Cristhofer <cristhofer.dev@gmail.com>
2020-05-21 00:17:37 -0500

    Single Page Application with Laravel: Introduction

---
+++ spa/resources/js/components/AboutComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "AboutComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ spa/resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <router-view></router-view>
+        <router-link to="/home">Home</router-link>
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>

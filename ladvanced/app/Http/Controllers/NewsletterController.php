92cd892df4cbd0ae7b0ed46e4c3969c5addd22d2
anomadev <cristhofer.dev@gmail.com>
2021-08-21 21:10:53 -0500

    PLatzi: Laravel Avanzado - La terminal de Laravel

---
+++ ladvanced/app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Console\Commands\SendNewsletterCommand;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+
+class NewsletterController extends Controller
+{
+    public function send()
+    {
+        Artisan::call(SendNewsletterCommand::class);
+        return response()->json([
+            'data' => 'Envio de correos completo'
+        ]);
+    }
+}

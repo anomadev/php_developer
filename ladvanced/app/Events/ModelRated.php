8c5ae2c984e6baa8b84730fd5bb068c7cccd47c4
anomadev <cristhofer.dev@gmail.com>
2021-08-21 22:08:28 -0500

    Platzi: Laravel Avanzado --Incompleto--

---
+++ ladvanced/app/Events/ModelRated.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ModelRated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    private Model $qualifier;
+    private Model $ratable;
+    private float $score;
+
+    public function __construct(Model $qualifier, Model $ratable, float $score)
+    {
+        $this->qualifier = $qualifier;
+        $this->ratable = $ratable;
+        $this->score = $score;
+    }
+
+    public function getQualifier(): Model
+    {
+        return $this->qualifier;
+    }
+
+    public function getRatable(): Model
+    {
+        return $this->ratable;
+    }
+
+    public function getScore(): float
+    {
+        return $this->score;
+    }
+}

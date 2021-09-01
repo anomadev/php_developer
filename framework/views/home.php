b5db063422bb326c14a6f011014b262764a24d48
anomadev <cristhofer.dev@gmail.com>
2021-09-01 18:05:55 -0500

    Platzi: Introduccion a Frameworks de PHP

---
+++ framework/views/home.php
@@ -0,0 +1,4 @@
+<div class="col-8">
+    <h1>Pagina Home</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, nulla tempore. Animi architecto cum debitis, ea eligendi labore laboriosam provident! Asperiores aspernatur corporis eligendi exercitationem facilis nostrum perferendis repellendus repudiandae!</p>
+</div>
---
+++ framework/views/layout.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Sitio Web</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        <div class="container">
+            <a href="/" class="navbar-brand h1">FW</a>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <?php echo $content; ?>
+        </div>
+    </div>
+</body>
+</html>
